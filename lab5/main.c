09dd76ad85b1f2e8c667579ee957e7f7a1a7244b
Elciney Junior <elcineyjr123@gmail.com>
2018-10-24 21:27:21 -0300

    melhorias no merge sort

--- lab5/main.c
+++ lab5/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern void merge_sort(Item *a, Item* aux, int lo, int hi);
+extern void sort(Item *a, int lo, int hi);
 
 Item* init_array(int N){
     Item* array = malloc(N * sizeof(Item));
@@ -15,10 +15,8 @@ int main(int argc, char *argv[]){
 
     for(int i = 0; i < N; i++) 
         scanf("%d", &array[i]); 
-
-    Item* aux_array = init_array(N);    
     
-    merge_sort(array, aux_array, 0, N-1); 
+    sort(array, 0, N-1); 
 
     for(int j = 0; j < N; j++)
         printf("%d\n", array[j]);
--- lab5/mergesort.c
+++ lab5/mergesort.c
@@ -3,6 +3,24 @@
 #include <stdbool.h>
 #include "item.h"
 
+#define CUTOFF 10
+
+
+void insertion_sort(Item* a, int lo, int hi){
+    for(int i  = hi; i > lo; i--)       // coloca o menor no inicio do vetor pra ser um sentinela
+        compexch(a[i-1], a[i]);
+
+    for(int i = lo+2; i <= hi; i++){
+        int j = i;
+        Item v = a[i];      //guarda v
+        while(less(v, a[j-1])){     //enquanto v for maior q os anteriores dele, joga todos pra frente
+            a[j] = a[j-1];
+            j--;
+        }
+        a[j] = v;   //insere o v na posiçao dele
+    }
+}
+
 
 void merge(Item* a, Item* aux, int lo, int mid, int hi){
     for(int k = 0; k <= hi; k++)     //copy the data to the aux array
@@ -20,17 +38,33 @@ void merge(Item* a, Item* aux, int lo, int mid, int hi){
 }
 
 
-
-
-
 void merge_sort(Item* a, Item* aux, int lo, int hi){
-    if(hi <= lo)
+    if(hi <= lo + CUTOFF - 1){
+        insertion_sort(a, lo, hi);
         return;
+    }
 
     int mid = lo + (hi - lo)/2;
 
     merge_sort(a, aux, lo, mid);
     merge_sort(a, aux, mid+1, hi);
 
+    //maior numero do vetor da esquerda eh menor ou igual ao menor do vetor da direita, ou seja, esta ordenado
+    if(!less(a[mid+1], a[mid]))   return;   
+    
     merge(a, aux, lo, mid, hi);    
+}
+
+
+
+
+
+
+void sort(Item* a, int lo, int hi){
+    int n = (hi - lo) + 1;
+    Item* aux = malloc(n * sizeof(Item));
+
+    merge_sort(a, aux, lo, hi);
+
+    free(aux);
 }
